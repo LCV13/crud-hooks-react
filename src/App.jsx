87d139e356b0e000fa162808e96f3b5a5eb0c84b
Lourdes C. Villarreal <58574759+LCV13@users.noreply.github.com>
2021-01-09 16:57:59 -0300

    estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD React</h1>
+      <hr/>
+      <div className="row">
+          <div className="col-8">
+            <h4 className="text-center">Lista de tareas</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <span className="lead">Nombre de la tarea</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right">Editar</button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <h4 className="text-center">Formulario</h4>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
